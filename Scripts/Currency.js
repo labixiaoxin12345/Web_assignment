7f75434e9c506fc400663f7c93940b8810014f96
labixiaoxin12345 <87477857+labixiaoxin12345@users.noreply.github.com>
2024-01-08 21:55:38 +0800

    Add files via upload

---
+++ Scripts/Currency.js
@@ -0,0 +1,31 @@
+﻿var appkey = '';
+
+$(function () {
+    appkey = '6a9f75e56be4f62d1af715f2';
+    //Bind Currency code dropdown
+    $.get("https://v6.exchangerate-api.com/v6/" + appkey + "/codes", {}, function (data) {
+        var row = "";
+        $.each(data.supported_codes, function (i, v) {
+            row += "<option value='" + v[0] + "'>" + v[0] + '-' + v[1] + "</option>";
+        });
+        $("#ddlform").append(row);
+        $("#ddlTo").append(row);
+        $("#ddlform").val('USD');
+        $("#ddlTo").val('EUR');
+        calculate();
+    }).fail(function (d) {
+        alert("Error while calling API");
+        console.log(d.responseText);
+    });
+});
+
+//calculate currency conversion
+function calculate() {
+    $.get("https://v6.exchangerate-api.com/v6/" + appkey + "/pair/" + $("#ddlform").val() + "/" + $("#ddlTo").val() + "/" + Number($("#txtamount").val()) + "", {}, function (data) {
+        $.get("#txtoutput").val(data.conversion_result);
+    }).fail(function (d) {
+        alert("Error while calling API");
+        console.log(d.responseText);
+    });
+
+}
